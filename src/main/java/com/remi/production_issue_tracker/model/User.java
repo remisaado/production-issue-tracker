9fa1b0dbc09f13656f51cae372d994dc449ef9bc
Remi Saado <remi_saado@hotmail.com>
2026-02-11 19:51:44 +0100

    Creates User class

---
+++ src/main/java/com/remi/production_issue_tracker/model/User.java
@@ -0,0 +1,32 @@
+package com.remi.production_issue_tracker.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public enum Role  {
+        OPERATOR,
+        TEAM_LEADER,
+        TECHNICIAN,
+        MANAGER
+    };
+
+    public long getId() { return id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public Role getRole() { return role; }
+    public void setRole(Role role) { this.role = role; }
+}
