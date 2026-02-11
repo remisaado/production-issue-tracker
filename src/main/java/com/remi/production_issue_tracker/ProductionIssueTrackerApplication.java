e741507dcdd2725b0040a29c2ad6a1f9613e23c4
Remi Saado <remi_saado@hotmail.com>
2026-02-11 19:46:20 +0100

    Initial commit.

---
+++ src/main/java/com/remi/production_issue_tracker/ProductionIssueTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.remi.production_issue_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductionIssueTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductionIssueTrackerApplication.class, args);
+	}
+
+}
