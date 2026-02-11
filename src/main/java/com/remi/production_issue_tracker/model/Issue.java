7e9fab9aae779ed8b32db214360adfa7ceddef8f
Remi Saado <remi_saado@hotmail.com>
2026-02-11 19:51:04 +0100

    Creates Issue class

---
+++ src/main/java/com/remi/production_issue_tracker/model/Issue.java
@@ -0,0 +1,70 @@
+package com.remi.production_issue_tracker.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Issue {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime createdAt;
+    private String title;
+    private String description;
+    @Enumerated(EnumType.STRING)
+    private IssueStatus status;
+    @Enumerated(EnumType.STRING)
+    private Priority priority;
+    @ManyToOne
+    @JoinColumn(name = "reported_by_id")
+    private User reportedBy;
+    @ManyToOne
+    @JoinColumn(name = "assigned_to_id")
+    private User assignedTo;
+    @ManyToOne
+    @JoinColumn(name = "production_line_id")
+    private ProductionLine productionLine;
+
+    public enum IssueStatus  {
+        OPEN,
+        ASSIGNED,
+        CLOSED,
+        SOLVED
+    };
+
+    public enum Priority  {
+        LOW,
+        MEDIUM,
+        HIGH,
+        CRITICAL
+    };
+
+    public Issue() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public Long getId() { return id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public IssueStatus getStatus() { return status; }
+    public void setStatus(IssueStatus status) { this.status = status; }
+
+    public Priority getPriority() { return priority; }
+    public void setPriority(Priority priority) { this.priority = priority; }
+
+    public LocalDateTime getCreatedAt() { return createdAt; }
+
+    public User getReportedBy() { return reportedBy; }
+    public void setReportedBy(User reportedBy) { this.reportedBy = reportedBy; }
+
+    public User getAssignedTo() { return assignedTo; }
+    public void setAssignedTo(User assignedTo) { this.assignedTo = assignedTo; }
+
+    public ProductionLine getProductionLine() { return productionLine; }
+    public void setProductionLine(ProductionLine productionLine) { this.productionLine = productionLine; }
+}
