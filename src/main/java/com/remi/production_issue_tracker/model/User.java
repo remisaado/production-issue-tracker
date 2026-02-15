52d3b31a45509420ec6bcf370e7a73cd1b8bd00f
Remi Saado <remi_saado@hotmail.com>
2026-02-15 12:44:09 +0100

    Fixes SQL syntax error by replacing reserved table name word

--- src/main/java/com/remi/production_issue_tracker/model/User.java
+++ src/main/java/com/remi/production_issue_tracker/model/User.java
@@ -3,6 +3,7 @@ package com.remi.production_issue_tracker.model;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
