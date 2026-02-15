0dc3d27391bf9acc024dd7e64090830afbb45e3a
Remi Saado <remi_saado@hotmail.com>
2026-02-15 16:02:33 +0100

    Creates UserService and UserController

---
+++ src/main/java/com/remi/production_issue_tracker/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.remi.production_issue_tracker.controller;
+
+import com.remi.production_issue_tracker.model.User;
+import com.remi.production_issue_tracker.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+}
---
+++ src/main/java/com/remi/production_issue_tracker/service/UserService.java
@@ -0,0 +1,30 @@
+package com.remi.production_issue_tracker.service;
+
+import com.remi.production_issue_tracker.model.User;
+import com.remi.production_issue_tracker.repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+    }
+}
