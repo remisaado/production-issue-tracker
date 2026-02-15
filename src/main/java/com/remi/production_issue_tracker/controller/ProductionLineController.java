c05ac464e71aac038eec23efc47ff58a1cf8949e
Remi Saado <remi_saado@hotmail.com>
2026-02-15 13:56:38 +0100

    Creates ProductionLineService and ProductionLineController with CRUD functionality

---
+++ src/main/java/com/remi/production_issue_tracker/controller/ProductionLineController.java
@@ -0,0 +1,41 @@
+package com.remi.production_issue_tracker.controller;
+
+import com.remi.production_issue_tracker.model.ProductionLine;
+import com.remi.production_issue_tracker.service.ProductionLineService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/production-lines")
+public class ProductionLineController {
+
+    private final ProductionLineService productionLineService;
+
+    public ProductionLineController(ProductionLineService productionLineService) { this.productionLineService = productionLineService; }
+
+    @GetMapping
+    public List<ProductionLine> getAllProductionLines() {
+        return productionLineService.getAllProductionLines();
+    }
+
+    @GetMapping("/{id}")
+    public ProductionLine getProductionLineById(@PathVariable Long id) {
+        return productionLineService.getProductionLineById(id);
+    }
+
+    @PostMapping
+    public ProductionLine CreateProductionLine(@RequestBody ProductionLine productionLine) {
+        return productionLineService.createProductionLine(productionLine);
+    }
+
+    @PutMapping("/{id}")
+    public ProductionLine updateProductionLine(@PathVariable Long id, @RequestBody ProductionLine productionLine) {
+        return productionLineService.updateProductionLine(id, productionLine);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProductionLine(@PathVariable Long id) {
+        productionLineService.deleteProductionLine(id);
+    }
+}
---
+++ src/main/java/com/remi/production_issue_tracker/service/ProductionLineService.java
@@ -0,0 +1,42 @@
+package com.remi.production_issue_tracker.service;
+
+import com.remi.production_issue_tracker.model.ProductionLine;
+import com.remi.production_issue_tracker.repository.ProductionLineRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Service
+public class ProductionLineService {
+    private final ProductionLineRepository productionLineRepository;
+
+    public ProductionLineService(ProductionLineRepository productionLineRepository) {
+        this.productionLineRepository = productionLineRepository;
+    }
+
+    public List<ProductionLine> getAllProductionLines() {
+        return productionLineRepository.findAll();
+    }
+
+    public ProductionLine createProductionLine(ProductionLine productionLine) {
+        return productionLineRepository.save(productionLine);
+    }
+
+    public ProductionLine getProductionLineById(Long id) {
+        return productionLineRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Production line not found"));
+    }
+
+    public void deleteProductionLine(Long id) {
+        productionLineRepository.deleteById(id);
+    }
+
+    public ProductionLine updateProductionLine(Long id, ProductionLine updatedProductionLine) {
+        ProductionLine existingProductionLine = productionLineRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Production line not found"));
+
+        existingProductionLine.setTitle(updatedProductionLine.getTitle());
+
+        return productionLineRepository.save(existingProductionLine);
+    }
+}
